860228cc5f050d240189c144090a1fbc9e45bd68
Ludovic Nascimbeni <ludovic.nascimbeni@gmail.com>
2016-03-04 17:33:26 +0100

    Redimensionnement des images

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,4 @@
-(function($){
+(function($) {
 
 	/* ---------------------------------------------- /*
 	 * Preloader
@@ -15,8 +15,8 @@
 		 * Smooth scroll / Scroll To Top
 		/* ---------------------------------------------- */
 
-		$('a[href*=#]').bind("click", function(e){
-           
+		$('a[href*=#]').bind("click", function(e) {
+
 			var anchor = $(this);
 			$('html, body').stop().animate({
 				scrollTop: $(anchor.attr('href')).offset().top
@@ -45,65 +45,69 @@
 			offset: 70
 		})
 
-        
-        /* ---------------------------------------------- /*
-		 * Skills
-        /* ---------------------------------------------- */    
-        //var color = $('#home').css('backgroundColor');
 
-        $('.skills').waypoint(function(){
-            $('.chart').each(function(){
-            $(this).easyPieChart({
-                    size:140,
-                    animate: 2000,
-                    lineCap:'butt',
-                    scaleColor: false,
-                    barColor: '#FF5252',
-                    trackColor: 'transparent',
-                    lineWidth: 10
-                });
-            });
-        },{offset:'80%'});
-        
-        
-        /* ---------------------------------------------- /*
+		/* ---------------------------------------------- /*
+		 * Skills
+        /* ---------------------------------------------- */
+		//var color = $('#home').css('backgroundColor');
+
+		$('.skills').waypoint(function() {
+			$('.chart').each(function() {
+				$(this).easyPieChart({
+					size: 140,
+					animate: 2000,
+					lineCap: 'butt',
+					scaleColor: false,
+					barColor: '#FF5252',
+					trackColor: 'transparent',
+					lineWidth: 10
+				});
+			});
+		}, {
+			offset: '80%'
+		});
+
+
+		/* ---------------------------------------------- /*
 		 * Quote Rotator
 		/* ---------------------------------------------- */
-       
-			$( function() {
-				/*
-				- how to call the plugin:
-				$( selector ).cbpQTRotator( [options] );
-				- options:
-				{
-					// default transition speed (ms)
-					speed : 700,
-					// default transition easing
-					easing : 'ease',
-					// rotator interval (ms)
-					interval : 8000
-				}
-				- destroy:
-				$( selector ).cbpQTRotator( 'destroy' );
-				*/
 
-				$( '#cbp-qtrotator' ).cbpQTRotator();
+		$(function() {
+			/*
+			- how to call the plugin:
+			$( selector ).cbpQTRotator( [options] );
+			- options:
+			{
+				// default transition speed (ms)
+				speed : 700,
+				// default transition easing
+				easing : 'ease',
+				// rotator interval (ms)
+				interval : 8000
+			}
+			- destroy:
+			$( selector ).cbpQTRotator( 'destroy' );
+			*/
 
-			} );
-		
-        
+			$('#cbp-qtrotator').cbpQTRotator();
+
+		});
+
+
 		/* ---------------------------------------------- /*
 		 * Home BG
 		/* ---------------------------------------------- */
 
 		$(".screen-height").height($(window).height());
 
-		$(window).resize(function(){
+		$(window).resize(function() {
 			$(".screen-height").height($(window).height());
 		});
 
 		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-			$('#home').css({'background-attachment': 'scroll'});
+			$('#home').css({
+				'background-attachment': 'scroll'
+			});
 		} else {
 			$('#home').parallax('50%', 0.1);
 		}
@@ -140,33 +144,31 @@
 			var c_email = $('#c_email').val();
 			var c_message = $('#c_message ').val();
 			var response = $('#contact-form .ajax-response');
-			
+
 			var formData = {
-				'name'       : c_name,
-				'email'      : c_email,
-				'message'    : c_message
+				'name': c_name,
+				'email': c_email,
+				'message': c_message
 			};
 
-			if (( c_name== '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email) )) {
+			if ((c_name == '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email))) {
 				response.fadeIn(500);
 				response.html('<i class="fa fa-warning"></i> Please fix the errors and try again.');
+			} else {
+				$.ajax({
+					type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
+					url: 'assets/php/contact.php', // the url where we want to POST
+					data: formData, // our data object
+					dataType: 'json', // what type of data do we expect back from the server
+					encode: true,
+					success: function(res) {
+						var ret = $.parseJSON(JSON.stringify(res));
+						response.html(ret.message).fadeIn(500);
+					}
+				});
 			}
-
-			else {
-					 $.ajax({
-							type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-							url         : 'assets/php/contact.php', // the url where we want to POST
-							data        : formData, // our data object
-							dataType    : 'json', // what type of data do we expect back from the server
-							encode      : true,
-							success		: function(res){
-											var ret = $.parseJSON(JSON.stringify(res));
-											response.html(ret.message).fadeIn(500);
-							}
-						});
-				}           
-            	return false;
-			});
+			return false;
+		});
 
 	});
 
